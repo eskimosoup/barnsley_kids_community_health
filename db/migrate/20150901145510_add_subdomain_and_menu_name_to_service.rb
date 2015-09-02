87ca4a6b70422cbcd8e3ef187f8745c6c37e550c
James Doyley <jdoyley@gmail.com>
2015-09-02 09:05:55 +0100

    Update migrations

--- db/migrate/20150901145510_add_subdomain_and_menu_name_to_service.rb
+++ db/migrate/20150901145510_add_subdomain_and_menu_name_to_service.rb
@@ -1,6 +1,6 @@
 class AddSubdomainAndMenuNameToService < ActiveRecord::Migration
   def change
-    add_column :services, :subdomain, :string, null: false
-    add_column :services, :menu_name, :string, null: false
+    add_column :services, :subdomain, :string
+    add_column :services, :menu_name, :string
   end
 end
